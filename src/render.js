0e09e8760bc5e95b289851fae38acbfacbca419a
Nuno Pereira <nunocgpereira@gmail.com>
2020-03-10 21:58:43 +0000

    add buttons

---
+++ src/render.js
@@ -0,0 +1,4 @@
+const videoElement = document.querySelector('video');
+const startBtn = document.getelementById('startBtn');
+const stopBtn = document.getelementById('stopBtn');
+const videoSelectBtn = document.getelementById('videoSelectBtn');
