9e9e8e93ba89cc1df30078eee6682faeaa874fa7
Nuno Pereira <nunocgpereira@gmail.com>
2020-03-10 22:14:35 +0000

    add window select function

--- src/App.js
+++ src/App.js
@@ -2,6 +2,35 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const videoElement = document.querySelector('video');
+const startBtn = document.getElementById('startBtn');
+const stopBtn = document.getElementById('stopBtn');
+const videoSelectBtn = document.getElementById('videoSelectBtn');
+videoSelectBtn.onclick = getVideoSources;
+
+const { desktopCapturer, remote } = require('electron');
+const { Menu } = remote;
+
+// Get available video sources
+async function getVideoSources() {
+  const inputSources = await desktopCapturer.getSources({
+    types: ['window', 'screen']
+  });
+
+  const videoOptionsMenu = Menu.buildFromTemplate(
+    inputSources.map(source => {
+      return {
+        label: source.name,
+        click: () => selectSource(source)
+      };
+    })
+  );
+
+  videoOptionsMenu.popup();
+}
+
+
+
 function App() {
   return (
     <div className="App">
--- src/render.js
+++ src/render.js
@@ -2,3 +2,25 @@ const videoElement = document.querySelector('video');
 const startBtn = document.getelementById('startBtn');
 const stopBtn = document.getelementById('stopBtn');
 const videoSelectBtn = document.getelementById('videoSelectBtn');
+videoSelectBtn.onclick = getVideoSources
+
+const { desktopCapturer, remote } = require('electron');
+const { Menu } = remote;
+
+// Get available video sources
+async function getVideoSources() {
+	const inputSources = await desktopCapturer.getSources({
+		types: ['window', 'screen']
+	});
+
+	const videoOptionsMenu = Menu.buildFromTemplate(
+		inputSources.map(source => {
+			return {
+				label: source.name,
+				click: () => selectSource(source)
+			};
+		})
+	);
+
+	videoOptionsMenu.popup();
+}
